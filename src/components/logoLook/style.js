e732faebbde69143c52f9389bdc04dd8c429ac41
Alexey25041991 <mazurenko.alexey@mail.ru>
2021-11-05 20:23:57 +0300

    Logo

--- src/components/logoLook/style.js
+++ src/components/logoLook/style.js
@@ -1,12 +1,4 @@
-import styled from 'styled-components';
-
-export const TopWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  height: 74px;
-  width: 960px;
-  margin: 0 auto;
-`;
+import styled from "styled-components";
 
 export const Logo = styled.div`
   display: flex;
@@ -20,7 +12,7 @@ export const LogoSign = styled.div`
   color: white;
   line-height: 30px;
   font-size: 25px;
-  font-family: 'Exo 2', sans-serif;
+  font-family: "Exo 2", sans-serif;
   font-weight: 800;
   margin-right: 14px;
   white-space: nowrap;
@@ -44,52 +36,5 @@ export const LogoCoderText = styled.div`
   font-weight: 400;
   padding-top: 4px;
   display: block;
-  font-family: 'Exo 2', sans-serif;
-`;
-
-export const Menu = styled.div`
-  padding-top: 10px;
-  float: right;
-`;
-
-export const Ul = styled.ul`
-  display: flex;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-`;
-
-export const Li = styled.li`
-  display: inline-block;
-  vertical-align: top;
-  text-align: center;
-  margin-bottom: 16px;
-
-  & :hover {
-    cursor: pointer;
-  }
-`;
-
-export const Link = styled.a`
-  text-decoration: none;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 5px 15px;
-  color: palevioletred;
-`;
-
-export const Label = styled.span`
-  display: flex;
-  align-items: center;
-  color: white;
-  line-height: 17px;
-  font-size: 14px;
-  font-family: 'Exo 2', sans-serif;
-  font-weight: 400;
-  text-decoration: none;
-  text-shadow: 1px 1px 0px rgba(0, 0, 0, 0.1);
-  text-transform: uppercase;
-  margin-top: 10px;
-  white-space: nowrap;
+  font-family: "Exo 2", sans-serif;
 `;
