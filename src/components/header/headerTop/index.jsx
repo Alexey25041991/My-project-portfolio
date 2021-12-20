65f9bde42a6460d31e5bdc6d13494cd19470d474
Alexey25041991 <mazurenko.alexey@mail.ru>
2021-12-20 15:01:56 +0300

    адаптив убрать дубирование

--- src/components/header/headerTop/index.jsx
+++ src/components/header/headerTop/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import LogoLook from "../../LogoLook";
-import MenuBurger from "./MenuBurger";
+import MenuBurger from "../../MenuBurger";
 
 import {
   Overlay,
