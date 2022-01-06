b284a3aa3a1c3b621e3524e3664e76980d08c8d2
Alexey25041991 <mazurenko.alexey@mail.ru>
2022-01-06 08:57:27 +0300

    Добавил в progressBar useLayoutEffect вроде меньше глючит оставил

--- src/components/common/ProgressBar/index.js
+++ src/components/common/ProgressBar/index.js
@@ -21,48 +21,6 @@ const ProgressBar = () => {
 
   const productsHref = getProductsHref();
 
-  // productsHref && window.addEventListener("scroll", progressBar);
-
-  // function progressBar(e) {
-  //   const progressTop = document.querySelector(".progress-top");
-  //   const progressRight = document.querySelector(".progress-right");
-  //   const progressBottom = document.querySelector(".progress-bottom");
-  //   const progressLeft = document.querySelector(".progress-left");
-
-  //   let windowScroll =
-  //     document.body.scrollTop || document.documentElement.scrollTop;
-  //   let windowHeight =
-  //     (document.documentElement.scrollHeight -
-  //       document.documentElement.clientHeight) /
-  //     4;
-  //   let per = (windowScroll / windowHeight) * 100;
-
-  //   if (per <= 100) {
-  //     progressTop.style.width = per.toFixed(0) + "%";
-  //     progressRight.style.height = "0%";
-  //     progressBottom.style.width = "0%";
-  //     progressLeft.style.height = "0%";
-  //   }
-  //   if (100 < per && per <= 200) {
-  //     progressTop.style.width = "100%";
-  //     progressRight.style.height = per.toFixed(0) - 100 + "%";
-  //     progressBottom.style.width = "0%";
-  //     progressLeft.style.height = "0%";
-  //   }
-  //   if (200 < per && per <= 300) {
-  //     progressTop.style.width = "100%";
-  //     progressRight.style.height = "100%";
-  //     progressBottom.style.width = per.toFixed(0) - 200 + "%";
-  //     progressLeft.style.height = "0%";
-  //   }
-  //   if (300 < per && per <= 400) {
-  //     progressTop.style.width = "100%";
-  //     progressRight.style.height = "100%";
-  //     progressBottom.style.width = "100%";
-  //     progressLeft.style.height = per.toFixed(0) - 295 + "%";
-  //   }
-  // }
-
   useLayoutEffect(() => {
     const progressBar = () => {
       const progressTop = document.querySelector(".progress-top");
