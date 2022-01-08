2d1710f95f5200fac85f32a5b12506abb037d987
Alexey25041991 <mazurenko.alexey@mail.ru>
2022-01-08 19:40:52 +0300

    Попробовал как в примере https://codepen.io/thomasvaeth/pen/XZQWMW

--- src/components/common/ProgressBar/index.js
+++ src/components/common/ProgressBar/index.js
@@ -28,50 +28,86 @@ const ProgressBar = () => {
       const progressBottom = document.querySelector(".progress-bottom");
       const progressLeft = document.querySelector(".progress-left");
 
-      let windowScroll =
-        window.pageYOffset || document.documentElement.scrollTop;
-      let windowHeight =
-        (document.documentElement.scrollHeight -
-          document.documentElement.clientHeight) /
-        4;
-      let per = ((windowScroll / windowHeight) * 100).toFixed(0);
-
-      let windowHeightPer =
+      let windowHeight = document.documentElement.scrollHeight;
+      let windowWidth = document.documentElement.scrollWidth;
+      let scrollHeight =
         document.documentElement.scrollHeight -
         document.documentElement.clientHeight;
-      let scrollPer = ((windowScroll / windowHeightPer) * 100).toFixed(0);
+      let progressTotal = windowHeight * 2 + windowWidth * 2;
+      let scrollPosition =
+        document.body.scrollTop || document.documentElement.scrollTop;
 
-      if (per <= 100) {
-        progressTop.style.width = per + "%";
-        progressRight.style.height = "0%";
-        progressBottom.style.width = "0%";
-        progressLeft.style.height = "0%";
-      }
-      if (100 < per && per <= 200) {
-        progressTop.style.width = "100%";
-        progressRight.style.height = per - 100 + "%";
-        progressBottom.style.width = "0%";
-        progressLeft.style.height = "0%";
-      }
-      if (200 < per && per <= 300) {
-        progressTop.style.width = "100%";
-        progressRight.style.height = "100%";
-        progressBottom.style.width = per - 200 + "%";
-        progressLeft.style.height = "0%";
-      }
-      if (300 < per && per <= 400) {
-        progressTop.style.width = "100%";
-        progressRight.style.height = "100%";
-        progressBottom.style.width = "100%";
-        progressLeft.style.height = per - 300 + "%";
-      }
-      if (97 <= scrollPer) {
-        progressTop.style.width = "100%";
-        progressRight.style.height = "100%";
-        progressBottom.style.width = "100%";
-        progressLeft.style.height = "100%";
-      }
+      let percentage = scrollPosition / scrollHeight;
+      let width = windowWidth / progressTotal;
+      let height = windowHeight / progressTotal;
+
+      let progressScrollTop = (percentage / width) * 100;
+      let progressScrollRight = ((percentage - width) / height) * 100;
+      let progressScrollBottom = ((percentage - width - height) / width) * 100;
+      let progressScrollLeft =
+        ((percentage - width - height - width) / height) * 100;
+
+      // console.log("progressScrollTop", progressScrollTop);
+      // console.log("progressScrollRight", progressScrollRight);
+      // console.log("progressScrollBottom", progressScrollBottom);
+      // console.log("progressScrollLeft", progressScrollLeft);
+
+      const setProgressScrollValue = (progressScrollValue) => {
+        // console.log(111, progressScrollValue);
+        return progressScrollValue > 0 ? `${progressScrollValue}%` : 0;
+      };
+
+      progressTop.style.width = setProgressScrollValue(progressScrollTop);
+      progressRight.style.height = setProgressScrollValue(progressScrollRight);
+      progressBottom.style.width = setProgressScrollValue(progressScrollBottom);
+      progressLeft.style.height = setProgressScrollValue(progressScrollLeft);
+
+      ///
+      //   let windowScroll =
+      //     document.body.scrollTop || document.documentElement.scrollTop;
+      //   let windowHeight =
+      //     (document.documentElement.scrollHeight -
+      //       document.documentElement.clientHeight) /
+      //     4;
+      //   let per = ((windowScroll / windowHeight) * 100).toFixed(0);
+
+      //   let windowHeightPer =
+      //     document.documentElement.scrollHeight -
+      //     document.documentElement.clientHeight;
+      //   let scrollPer = ((windowScroll / windowHeightPer) * 100).toFixed(0);
+
+      //   if (per <= 100) {
+      //     progressTop.style.width = per + "%";
+      //     progressRight.style.height = "0%";
+      //     progressBottom.style.width = "0%";
+      //     progressLeft.style.height = "0%";
+      //   }
+      //   if (100 < per && per <= 200) {
+      //     progressTop.style.width = "100%";
+      //     progressRight.style.height = per - 100 + "%";
+      //     progressBottom.style.width = "0%";
+      //     progressLeft.style.height = "0%";
+      //   }
+      //   if (200 < per && per <= 300) {
+      //     progressTop.style.width = "100%";
+      //     progressRight.style.height = "100%";
+      //     progressBottom.style.width = per - 200 + "%";
+      //     progressLeft.style.height = "0%";
+      //   }
+      //   if (300 < per && per <= 400) {
+      //     progressTop.style.width = "100%";
+      //     progressRight.style.height = "100%";
+      //     progressBottom.style.width = "100%";
+      //     progressLeft.style.height = per - 300 + "%";
+      //   }
+      //   if (97 <= scrollPer) {
+      //     progressTop.style.width = "100%";
+      //     progressRight.style.height = "100%";
+      //     progressBottom.style.width = "100%";
+      //     progressLeft.style.height = "100%";
+      //   }
     };
+    ///
 
     productsHref && document.addEventListener("scroll", progressBar);
     return () => {
