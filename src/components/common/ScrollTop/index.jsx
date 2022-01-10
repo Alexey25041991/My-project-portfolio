ebbbedc569b3ffab2fb083b1a7f13ee0a49bc689
Alexey25041991 <mazurenko.alexey@mail.ru>
2022-01-10 11:31:38 +0300

    Скролл вверх ракеты

--- src/components/common/ScrollTop/index.jsx
+++ src/components/common/ScrollTop/index.jsx
@@ -1,14 +1,30 @@
 import React, { useLayoutEffect, useState } from "react";
 
+import { animate } from "../utils";
+
 import { ScrollTopWrapper } from "./style";
+
+import "./style.css";
 
 // import { ReactComponent as RocketScrollToIcon } from "../icon/RocketScrollToIcon.png";
 
 const ScrollTop = () => {
   const [handleScrollTop, setHandleScrollTop] = useState(false);
 
+  const boxScrollTop = document?.querySelector(".scroll-top");
+
   const handleClick = () => {
-    window.scrollTo(0, 0);
+    animate({
+      duration: 2000,
+      timing(timeFraction) {
+        return Math.pow(timeFraction, 5);
+      },
+      draw(progress) {
+        if (boxScrollTop && boxScrollTop.style.bottom)
+          boxScrollTop.style.bottom = progress * 110 + "%";
+        window.scrollBy(0, -progress * 1000);
+      },
+    });
   };
 
   useLayoutEffect(() => {
@@ -16,16 +32,23 @@ const ScrollTop = () => {
       let scrolled = window.pageYOffset;
       let coords = document.documentElement.clientHeight;
       setHandleScrollTop(scrolled > coords);
+
+      if (boxScrollTop && boxScrollTop.style.bottom)
+        boxScrollTop.style.bottom = "30px";
     };
+
     document.addEventListener("scroll", handleScroll);
     return () => {
       document.removeEventListener("scroll", handleScroll);
     };
-  }, [setHandleScrollTop]);
+  });
 
   return (
     handleScrollTop && (
-      <ScrollTopWrapper onClick={handleClick}></ScrollTopWrapper>
+      <ScrollTopWrapper
+        onClick={handleClick}
+        className="scroll-top"
+      ></ScrollTopWrapper>
     )
   );
 };
--- src/components/common/utils/index.js
+++ src/components/common/utils/index.js
@@ -1,3 +1,4 @@
 export { getMobileOperatingSystem } from "./getMobileOperatingSystem";
 export { isAndroid } from "./isAndroid";
 export { isIos } from "./isIos";
+export { animate } from "./animate";
