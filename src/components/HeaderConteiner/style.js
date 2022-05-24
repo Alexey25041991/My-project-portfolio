59552edd59ac905728c14529bef814724ca9a4fd
Alexey25041991 <mazurenko.alexey@mail.ru>
2022-05-24 11:03:54 +0300

    Поменял светлый цвет header

--- src/components/HeaderConteiner/style.js
+++ src/components/HeaderConteiner/style.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-  background-color: #2b3037;
+  // background-color: #2b3037;
+  background-color: SlateGrey;
 `;
