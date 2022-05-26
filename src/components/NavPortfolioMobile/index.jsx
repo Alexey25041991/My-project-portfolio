f6ac992e68e0b48aa54a048d41453e4a25ef87b8
Alexey25041991 <mazurenko.alexey@mail.ru>
2022-05-26 17:01:41 +0300

    Поправил навигацию для мобильной версии

--- src/components/NavPortfolioMobile/index.jsx
+++ src/components/NavPortfolioMobile/index.jsx
@@ -1,4 +1,9 @@
 import React, { Fragment, useEffect, useState } from "react";
+import { observer } from "mobx-react";
+
+import { store } from "../../store";
+
+import { Link as LinkHref } from "react-router-dom";
 
 import MenuBurger from "../MenuBurger";
 import ButtonLink from "../common/ButtonLink";
@@ -11,6 +16,7 @@ import {
   Ul,
   Li,
   Link,
+  LinkDiv,
   Button,
   Label,
   MenuWrapper,
@@ -47,84 +53,112 @@ const NavMenuLi = (propsList, setPortfoliosValue, setOpened, navMenuLink) =>
     );
   });
 
-const HeaderMenuLi = (propsList, setOpened, header) =>
-  propsList.map((item) => (
-    <Li header={header} key={item.id} onClick={() => setOpened(false)}>
-      <Link href={`${"/#" + item.value}`}>
-        {item.icon}
-        <Label style={{ pointerEvents: "none" }}>{item.label}</Label>
-      </Link>
-    </Li>
-  ));
+const HeaderMenuLi = (propsList, setOpened, header, routeLink) => {
+  const propsListValue = propsList.slice(1);
+  const routeLinkBlog = routeLink.startsWith("blog");
+  const routeLinkPortfolio = routeLink.startsWith("portfolio");
+  const arrPropsList =
+    routeLinkBlog || routeLinkPortfolio
+      ? propsListValue.slice(-3)
+      : propsListValue;
 
-const NavPortfolioMobile = ({
-  children,
-  propsList,
-  setPortfoliosValue,
-  header,
-  navMenuLink,
-}) => {
-  const [opened, setOpened] = useState(false);
-
-  const onBlur = (e) => {
-    if (!e.currentTarget.contains(e.relatedTarget)) {
-      setOpened(false);
-    }
+  const scrollToBottom = (id) => {
+    const element = document?.getElementById(id);
+    window.scrollTo(0, document.body?.scrollHeight - element?.scrollHeight);
   };
 
-  useEffect(() => {
-    opened
-      ? (document.body.style.overflow = "hidden")
-      : (document.body.style.overflow = "");
-  }, [opened]);
+  return arrPropsList.map((item) => (
+    <Li header={header} key={item.id} onClick={() => setOpened(false)}>
+      {!item.href && item.value !== "contacts" && (
+        <Link href={`${"/#" + item.value}`}>
+          {item.icon}
+          <Label style={{ pointerEvents: "none" }}>{item.label}</Label>
+        </Link>
+      )}
+      {item.href && (
+        <LinkHref to={item.href}>
+          <LinkDiv>
+            {item.icon}
+            <Label style={{ pointerEvents: "none" }}>{item.label}</Label>
+          </LinkDiv>
+        </LinkHref>
+      )}
+      {item.value === "contacts" && (
+        <LinkDiv onClick={() => scrollToBottom(item.value)}>
+          {item.icon}
+          <Label style={{ pointerEvents: "none" }}>{item.label}</Label>
+        </LinkDiv>
+      )}
+    </Li>
+  ));
+};
 
-  const MenuLi = header
-    ? HeaderMenuLi(propsList, setOpened, header)
-    : NavMenuLi(propsList, setPortfoliosValue, setOpened, navMenuLink);
+const NavPortfolioMobile = observer(
+  ({ children, propsList, setPortfoliosValue, header, navMenuLink }) => {
+    const [opened, setOpened] = useState(false);
 
-  return (
-    <>
-      <Overlay data-close-border opened={opened} />
-      <HeaderTopWrapper
-        data-close-modal
-        header={header}
-        onBlur={onBlur}
-        tabIndex={1}
-      >
-        <MenuWrapper header={header}>
-          {children}
-          {header && (
-            <ContainerWrapper>
+    const routeLink = store.getRouteLink();
+
+    const onBlur = (e) => {
+      if (!e.currentTarget.contains(e.relatedTarget)) {
+        setOpened(false);
+      }
+    };
+
+    useEffect(() => {
+      opened
+        ? (document.body.style.overflow = "hidden")
+        : (document.body.style.overflow = "");
+    }, [opened]);
+
+    const MenuLi = header
+      ? HeaderMenuLi(propsList, setOpened, header, routeLink)
+      : NavMenuLi(propsList, setPortfoliosValue, setOpened, navMenuLink);
+
+    return (
+      <>
+        <Overlay data-close-border opened={opened} />
+        <HeaderTopWrapper
+          data-close-modal
+          header={header}
+          onBlur={onBlur}
+          tabIndex={1}
+        >
+          <MenuWrapper header={header}>
+            {children}
+            {header && (
+              <ContainerWrapper>
+                <MenuBurger
+                  opened={opened}
+                  handleClick={() => setOpened(!opened)}
+                />
+                <PopupSetting positionValue="bottom center">
+                  <ContainerWrapper>
+                    <ProfileWrapper>
+                      <ProfileIcon fill="white" />
+                    </ProfileWrapper>
+                    <SettingWrapper>
+                      <SettingIcon fill="white" />
+                    </SettingWrapper>
+                  </ContainerWrapper>
+                </PopupSetting>
+              </ContainerWrapper>
+            )}
+            {!header && (
               <MenuBurger
                 opened={opened}
                 handleClick={() => setOpened(!opened)}
               />
-              <PopupSetting positionValue="bottom center">
-                <ContainerWrapper>
-                  <ProfileWrapper>
-                    <ProfileIcon fill="white" />
-                  </ProfileWrapper>
-                  <SettingWrapper>
-                    <SettingIcon fill="white" />
-                  </SettingWrapper>
-                </ContainerWrapper>
-              </PopupSetting>
-            </ContainerWrapper>
-          )}
-          {!header && (
-            <MenuBurger
-              opened={opened}
-              handleClick={() => setOpened(!opened)}
-            />
-          )}
-        </MenuWrapper>
-        <HeaderMenu opened={opened}>
-          <Ul>{MenuLi}</Ul>
-        </HeaderMenu>
-      </HeaderTopWrapper>
-    </>
-  );
-};
+            )}
+          </MenuWrapper>
+          <HeaderMenu opened={opened}>
+            <Ul>{MenuLi}</Ul>
+          </HeaderMenu>
+        </HeaderTopWrapper>
+      </>
+    );
+  }
+);
 
 NavPortfolioMobile.defaultProps = {
   setPortfoliosValue: () => {},
--- src/components/NavPortfolioMobile/style.js
+++ src/components/NavPortfolioMobile/style.js
@@ -166,6 +166,17 @@ export const Link = styled.a`
   padding: 10px 20px;
 `;
 
+export const LinkDiv = styled.div`
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 5px 8px;
+  color: palevioletred;
+  flex-direction: row;
+  padding: 10px 20px;
+`;
+
 export const Button = styled.div`
   text-decoration: none;
   display: flex;
